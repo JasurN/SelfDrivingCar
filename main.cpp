0c850a4db5889a84c69a7798c574c7089ca6ea74
jasurn <97jasur@gmail.com>
2019-03-06 18:09:44 +0500

    -added CMakeLists to the project to support OpenCv

--- main.cpp
+++ main.cpp
@@ -1,6 +1,16 @@
+#include <opencv2/core/core.hpp>
+#include <opencv2/imgcodecs.hpp>
+#include <opencv2/highgui/highgui.hpp>
 #include <iostream>
+#include <string>
+
+using namespace cv;
+using namespace std;
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    Mat image = imread("image.jpg"); // Read the file
+    namedWindow("Display window", WINDOW_AUTOSIZE); // Create a window for display.
+    imshow("Display window", image); // Show our image inside it.
+    waitKey(0); // Wait for a keystroke in the window
     return 0;
 }
