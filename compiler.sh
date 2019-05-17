b58d69bf8d15297d9fe671d7f65b00ae0459140d
Jasurn <97jasur@gmail.com>
2019-05-17 16:13:38 +0000

    adjust code to raspberry

--- compiler.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-# Script to compile OpenCv c++ file. Pass file name as first argument. 
-
-EDIT The fastest and easiest way is nbind. If you want to write asynchronous add-on you can combine Asyncworker class from nan.
-
