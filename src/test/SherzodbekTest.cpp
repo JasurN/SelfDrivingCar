14f9f50de7eb4f0df7426c3bc09b2e5293a30e8d
jasurn <97jasur@gmail.com>
2019-05-18 22:48:26 +0500

    -after adding thread

--- src/test/SherzodbekTest.cpp
+++ src/test/SherzodbekTest.cpp
@@ -1,50 +1,47 @@
-#include <stdio.h>
 #include <wiringPi.h>
-#include <string.h>
 #include <softPwm.h>
 #include <iostream>
 #include <opencv/cv.h>
 #include "opencv2/opencv.hpp"
 #include "opencv2/imgproc.hpp"
-#include <raspicam/raspicam_cv.h>
 
 using namespace cv;
 using namespace std;
 
-
-#define LEFT_TRACER_PIN				10			//- IRTracer
-#define RIGHT_TRACER_PIN			11
-#define TRIG_PIN				28				//Ultrasonic
-#define ECHO_PIN				29
-#define IN1_PIN		1							// left motor
-#define IN2_PIN		4							// left motor
-#define IN3_PIN		5							// right motor
-#define IN4_PIN		6							// right motor
-#define LIMIT_DISTANCE  20						//Ultra limit distance for stop
-#define NORMAL_SPEED   50							//Speed limit
+#define LEFT_TRACER_PIN                10            //- IRTracer
+#define RIGHT_TRACER_PIN            11
+#define TRIG_PIN                28                //Ultrasonic
+#define ECHO_PIN                29
+#define IN1_PIN        1                            // left motor
+#define IN2_PIN        4                            // left motor
+#define IN3_PIN        5                            // right motor
+#define IN4_PIN        6                            // right motor
+#define LIMIT_DISTANCE  20                        //Ultra limit distance for stop
+#define NORMAL_SPEED   50                            //Speed limit
 
 #define MIN_SPEED   0
 
 void initSensorsDCMotor();
+
+//motor Control
 void goForward();
+
 void goLeft();
+
 void goRight();
+
 void stopDCMotor();
-void checkControl();
+
+//ultrasonic
+void *checkControl(void *threadarg);
+
 int getDistance();
-void setup();
 
-
-// Global variables for traffic light detection
-static unsigned char LINE[] = {21,22,26};
-
-
-
-int main()
-{
-    if(wiringPiSetup() == -1)
+//thread function
+int main() {
+    if (wiringPiSetup() == -1)
         return 0;
-
+    pthread_t pthreads[2];
     pinMode(IN1_PIN, OUTPUT);
     pinMode(IN2_PIN, OUTPUT);
     pinMode(IN3_PIN, OUTPUT);
@@ -52,27 +49,20 @@ int main()
 
     initSensorsDCMotor();
 
-
-    setup();
-
-    cout<<"\nCamera Started\n";
     stopDCMotor();
-
-    while(1)
-    {
-        checkControl();
+    int sensor_control = pthread_create(&pthreads[0], nullptr, checkControl, (void *) 1);
+    if (sensor_control) {
+        std::cout << "Error:unable to create sensor  thread," << sensor_control << std::endl;
     }
 
-
-
+    pthread_exit(nullptr);
     return 0;
 }
 
-void initSensorsDCMotor()
-{
+void initSensorsDCMotor() {
     pinMode(TRIG_PIN, OUTPUT);
     pinMode(ECHO_PIN, INPUT);
-    pinMode(LEFT_TRACER_PIN,   INPUT);
+    pinMode(LEFT_TRACER_PIN, INPUT);
     pinMode(RIGHT_TRACER_PIN, INPUT);
 
     pinMode(IN1_PIN, SOFT_PWM_OUTPUT);
@@ -87,8 +77,7 @@ void initSensorsDCMotor()
 }
 
 //Go Forward
-void goForward()
-{
+void goForward() {
 
     softPwmWrite(IN1_PIN, NORMAL_SPEED);
     softPwmWrite(IN2_PIN, MIN_SPEED);
@@ -99,8 +88,7 @@ void goForward()
 }
 
 //Go Backward
-void goBackward()
-{
+void goBackward() {
     softPwmWrite(IN1_PIN, MIN_SPEED);
 
     softPwmWrite(IN2_PIN, NORMAL_SPEED);
@@ -111,8 +99,7 @@ void goBackward()
 }
 
 //Go Left
-void goLeft()
-{
+void goLeft() {
 
     softPwmWrite(IN1_PIN, MIN_SPEED);
     softPwmWrite(IN2_PIN, NORMAL_SPEED);
@@ -123,8 +110,7 @@ void goLeft()
 }
 
 //Go Right
-void goRight()
-{
+void goRight() {
 
     softPwmWrite(IN1_PIN, NORMAL_SPEED);
     softPwmWrite(IN2_PIN, MIN_SPEED);
@@ -135,8 +121,7 @@ void goRight()
 }
 
 //Stop DC Motor
-void stopDCMotor()
-{
+void stopDCMotor() {
 
     softPwmWrite(IN1_PIN, MIN_SPEED);
     softPwmWrite(IN2_PIN, MIN_SPEED);
@@ -147,65 +132,56 @@ void stopDCMotor()
 }
 
 //Get distance from ultrasonic
-int getDistance()
-{
-    int 		start_time=0, end_time=0;
-    float 	distance=0;
+int getDistance() {
+    int start_time = 0, end_time = 0;
+    float distance = 0;
 
-    digitalWrite(TRIG_PIN, LOW) ;
-    delay(5) ;
-    digitalWrite(TRIG_PIN, HIGH) ;
-    delayMicroseconds(10) ;
-    digitalWrite(TRIG_PIN, LOW) ;
+    digitalWrite(TRIG_PIN, LOW);
+    delay(5);
+    digitalWrite(TRIG_PIN, HIGH);
+    delayMicroseconds(10);
+    digitalWrite(TRIG_PIN, LOW);
 
-    while (digitalRead(ECHO_PIN) == 0) ;
-    start_time = micros() ;
+    while (digitalRead(ECHO_PIN) == 0);
+    start_time = micros();
 
-    while (digitalRead(ECHO_PIN) == 1) ;
-    end_time = micros() ;
+    while (digitalRead(ECHO_PIN) == 1);
+    end_time = micros();
 
-    distance = (end_time - start_time) / 29. / 2. ;
+    distance = (end_time - start_time) / 29. / 2.;
 
-    return (int)distance;
+    return (int) distance;
 }
 
 
 //IR tracker
-void checkControl()
-{
-    int  nLValue = digitalRead(LEFT_TRACER_PIN);
-    int  nRValue = digitalRead(RIGHT_TRACER_PIN);
+void *checkControl(void *threadarg) {
+    while (true){
+    int nLValue = digitalRead(LEFT_TRACER_PIN);
+    int nRValue = digitalRead(RIGHT_TRACER_PIN);
     int dis = getDistance();
 
-    if( (nLValue == HIGH) && (nRValue == HIGH))
-    {
-        if( dis <= LIMIT_DISTANCE){
-            printf("distance - %d cm\n", dis) ;
+    if ((nLValue == HIGH) && (nRValue == HIGH)) {
+        if (dis <= LIMIT_DISTANCE) {
+            printf("distance - %d cm\n", dis);
 
             stopDCMotor();
-        }else{
+        } else {
             goForward();
 
 
-
         }
-    }else if( nLValue == HIGH){
+    } else if (nLValue == HIGH) {
         printf(" LEFT detect ~!!! MOVE  ");
         goLeft();
-    }else if(nRValue == HIGH){
+    } else if (nRValue == HIGH) {
         printf(" RIGHT detect ~!!! MOVE  ");
         goRight();
-    }else{
+    } else {
         stopDCMotor();
+    }
     }
 }
 
-void setup(){
-    wiringPiSetup();
-    int i;
-    for(i=0; i<sizeof(LINE); i++){
-        pinMode(LINE[i], INPUT);
-    }
-}
 
 
