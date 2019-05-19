4d94a0b26c5d7afe92ddb01b250ddb4698dd60f5
jasurn <97jasur@gmail.com>
2019-05-19 13:22:52 +0500

    trying to fix error

--- src/main.cpp
+++ src/main.cpp
@@ -19,7 +19,7 @@ using namespace std;
 #define IN3_PIN        5                            // right motor
 #define IN4_PIN        6                            // right motor
 #define LIMIT_DISTANCE  20                        //Ultra limit distance for stop
-#define NORMAL_SPEED   40                            //Speed limit
+#define NORMAL_SPEED   50                            //Speed limit
 
 #define MIN_SPEED   0
 
